2940ba1f91c1afc869f03fd825a30ecd8114669f
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-23 12:36:50 +0200

    db

--- webapp-travel-agency/Controllers/ViaggiController.cs
+++ webapp-travel-agency/Controllers/ViaggiController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webapp_travel_agency.Data;
+using webapp_travel_agency.Models;
 
 namespace webapp_travel_agency.Controllers
 {
@@ -8,5 +10,47 @@ namespace webapp_travel_agency.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormPost");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Viaggio nuovoViaggio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPost", nuovoViaggio);
+            }
+
+            using (ViaggioContext db = new ViaggioContext())
+            {
+                Viaggio viaggioDaCreare = new Viaggio(nuovoViaggio.Nome, nuovoViaggio.Prezzo, nuovoViaggio.Descrizione, nuovoViaggio.Foto);
+
+                db.Viaggios.Add(viaggioDaCreare);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("HomePage");
+        }
+
+        public IActionResult ListaViaggi()
+        {
+
+            List<Viaggio> listaViaggi = new List<Viaggio>();
+
+            using(ViaggioContext db = new ViaggioContext())
+            {
+
+                listaViaggi = db.Viaggios.ToList<Viaggio>();
+
+            }
+            return View("ListaViaggi", listaViaggi);
+
+        }
     }
+
 }
