1c0680672fb5118d212b657c00c5bbf1b6366ae6
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-23 11:32:47 +0200

    Home page amministrativa completata

---
+++ webapp-travel-agency/Controllers/ViaggiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_travel_agency.Controllers
+{
+    public class ViaggiController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
