800f23e905eb979fee55d91a2ec37f67a4edfce1
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-23 11:47:52 +0200

    Database con tabella creato

---
+++ webapp-travel-agency/Data/ViaggioContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Data
+{
+    public class ViaggioContext : DbContext
+    {
+
+        public DbSet<Viaggio> Viaggios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+
+            optionBuilder.UseSqlServer("Data Source=localhost; Database=viaggi_db; Integrated Security= true");
+
+
+        }
+
+    }
+}
---
+++ webapp-travel-agency/Models/Viaggio.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp_travel_agency.Models
+{
+    public class Viaggio
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(15, ErrorMessage = "Il nome deve essere di massimo 15 caratteri")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Il prezzo è obbligatorio")]
+        public double Prezzo { get; set; }
+        [Required(ErrorMessage = "descrizione è obbligatorio")]
+        public string Descrizione { get; set; }
+        [Required(ErrorMessage = " Il link della foto è obbligatorio")]
+        [Url(ErrorMessage = "Devi inserire un Url")]
+        public string Foto { get; set; }
+        public Viaggio()
+        {
+
+
+
+        }
+
+        public Viaggio(string nome, double prezzo, string descrizione, string foto)
+        {
+
+            this.Nome = nome;
+            this.Prezzo = prezzo;
+            this.Descrizione = descrizione;
+            this.Foto = foto;
+
+        }
+
+    }
+}
